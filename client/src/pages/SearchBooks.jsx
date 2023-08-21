c7f3fd466e2e071df1d6d934ef2628bb18142d14
Lalu423 <>
2023-08-21 19:19:43 -0400

    refactored pages to .jsx

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -7,10 +7,12 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
-
+import { useMutation } from "@apollo/client"
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from "../utils/mutations";
+import { GET_ME } from "../utils/queries";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,6 +22,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, {error}] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -72,9 +76,17 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      await saveBook({
+        variables: {
+          bookToSave: bookToSave
+        },
+        update: (cache, {data}) => {
 
-      if (!response.ok) {
+        },
+        refetchQueries: [{ query: GET_ME }],
+      });
+
+      if (error) {
         throw new Error('something went wrong!');
       }
 
